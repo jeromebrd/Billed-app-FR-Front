8a1b662c872d03dd1c90786afc0e158fdb77bb21
Jerome <jerome.bourscheid@gmail.com>
2024-03-13 18:04:36 +0100

    __tests__ > Bills.js -> the first test is verified with "expect"

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -30,7 +30,8 @@ describe('Given I am connected as an employee', () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId('icon-window'));
       const windowIcon = screen.getByTestId('icon-window');
-      //to-do write expect expression
+      //windowIcon should contains activ-icon class
+      expect(windowIcon.classList.contains('active-icon')).toBeTruthy();
     });
     test('Then bills should be ordered from earliest to latest', () => {
       document.body.innerHTML = BillsUI({ data: bills });
